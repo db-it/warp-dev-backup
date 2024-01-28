9bf77bbe26a2655a98221fa1f3105d4dcd4ba163
Dominik Büttner <mail@db-it.de>
2024-01-28 08:18:45 +0100

    Add tmutil module

---
+++ src/warp_dev_backup/tmutil.py
@@ -0,0 +1,29 @@
+import logging
+import re
+import subprocess
+
+
+class TMUtil:
+
+    @classmethod
+    def path_is_excluded(cls, path):
+        result = subprocess.run(['tmutil', 'isexcluded', path], stdout=subprocess.PIPE)
+        tmutil_isexcluded_stdout = result.stdout.decode()
+        logging.debug(f"Subprocess result: {result}")
+        match = re.search(r'\[Excluded]', tmutil_isexcluded_stdout)
+        return match
+
+    @classmethod
+    def exclude_path(cls, path, context):
+        if cls.path_is_excluded(path):
+            logging.info(f"Path is already excluded: {path}")
+            if context.v > 0:
+                print(f"Path is already excluded: {path}")
+        else:
+            result = subprocess.run(['tmutil', 'addexclusion', path], stdout=subprocess.PIPE)
+            if result.returncode == 0:
+                logging.info(f"Excluded path: {path}")
+            else:
+                logging.error(result.stdout)
+                logging.error(result.stderr)
+            return result.returncode
